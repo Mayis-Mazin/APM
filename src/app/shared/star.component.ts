d2b65415b66f5ee74fc6a346e9f552b76f6541b5
Mayis-Mazin <mayis.mazin@gmail.com>
2021-03-25 02:53:43 +0200

    Using a Nested Component: STAR Component

--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnChanges } from "@angular/core";
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  Output,
+} from '@angular/core';
 
 @Component({
-    selector:'pm-star',
-    template:'./star.component.html',
-    styleUrls:['./star.component.css']
+  selector: 'pm-star',
+  templateUrl: './star.component.html',
+  styleUrls: ['./star.component.css'],
 })
-export class StarComponent implements OnChanges{
-    rating:number=4;
-    cropWidth:number=75;
+export class StarComponent implements OnChanges {
+  rating: number = 4;
+  cropWidth = 75;
 
-    ngOnChanges():void{
-        this.cropWidth=this.rating*75/5
-    }
+  ngOnChanges(): void {
+    this.cropWidth = (this.rating * 75) / 5;
+  }
 }
